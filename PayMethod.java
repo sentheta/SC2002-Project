5a93b43c9556c6821d9a3de956efea0393d81d66
Jiaxin1145 <165999841+Jiaxin1145@users.noreply.github.com>
2024-04-06 03:45:30 +0800

    Update PayMethod.java

--- PayMethod.java
+++ PayMethod.java
@@ -1,6 +1,18 @@
 import java.io.*;
 import java.util.*;
 
-public class PayMethod{
-	
+public class PayMethod {
+    private String name;
+   
+    public PayMethod(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+    public void pay(double amount) {
+        System.out.println("Payment of $" + amount + " made using " + name);
+    }
 }
