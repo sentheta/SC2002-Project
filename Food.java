e6171a6d4d98090e8ba208f330f27eb86af0c6b0
Rugmasethu <166004855+Rugmasethu@users.noreply.github.com>
2024-04-08 04:58:21 +0800

    Update Food.java

---
+++ Food.java
@@ -0,0 +1,47 @@
+    private String name;
+    private boolean availability;
+    private double price;
+    private String category;
+
+    
+    public Food(String name, boolean availability, double price, String category) {
+        this.name = name;
+        this.availability = availability;
+        this.price = price;
+        this.category = category;
+    }
+
+  
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean getAvailable() {
+        return availability;
+    }
+
+    public void setAvailable(boolean availability) {
+        this.availability = availability;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
