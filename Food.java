2bedb357c14374885332dd12e42fbb980c952140
sentheta <80050863+sentheta@users.noreply.github.com>
2024-04-08 09:21:46 +0800

    Update Food.java
    Added class{} at the beginning

--- Food.java
+++ Food.java
@@ -1,3 +1,7 @@
+import java.io.*;
+import java.util.*;
+
+public class Food{
     private String name;
     private boolean availability;
     private double price;
@@ -11,37 +15,29 @@
         this.category = category;
     }
 
-  
-
+ 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public boolean getAvailable() {
         return availability;
     }
-
     public void setAvailable(boolean availability) {
         this.availability = availability;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
-
     public String getCategory() {
-        return category;
+    	return category;
     }
-
-    public void setCategory(String category) {
-        this.category = category;
+    public void setCategory(String category){
+    	this.category = category;
     }
 }
