cce6e726b0a2434613a2330e2a7f122088550799
sentheta <80050863+sentheta@users.noreply.github.com>
2024-04-08 09:56:23 +0800

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,6 @@
 interface Person{
-
+	// Print list of action and lets user choose ONE action
+	// Return TRUE if some action is done
+	// Return FALSE if user choose to end the session
+	public boolean chooseAction();
 }
