499dd53b4098834d5d8117f19959d68beec20941
ChuaCleon <164314024+ChuaCleon@users.noreply.github.com>
2024-04-25 21:44:39 +0800

    Update PayCash.java

--- PayCash.java
+++ PayCash.java
@@ -1,13 +1,18 @@
-package FOODIE;
+package scse_FOODIE;
 import java.util.*;
 import java.text.*;
 import java.io.Serializable;
-
-class PayCash extends PayMethod implements Serializable{
+/**
+ * PayCash class simulates payment by a customer using cash.
+ * Extends the PayMethod class. 
+ */
+public class PayCash extends PayMethod implements Serializable{
     
     //================================================================//
     //================================================================//
-
+    /**
+	 * Default constructor creating PayCash object with name Cash.
+	 */
     PayCash(){
         super("Cash");
     }
