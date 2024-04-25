224fa970a6d6ebc9d619ff3ca075a76a9253a11a
ChuaCleon <164314024+ChuaCleon@users.noreply.github.com>
2024-04-25 21:23:49 +0800

    Update IActionable.java

--- IActionable.java
+++ IActionable.java
@@ -1,7 +1,15 @@
-package FOODIE;
+package scse_FOODIE;
+/**
+ * An interface representing objects that can perform certain actions.
+ */
 interface IActionable{
 	// Print list of action and lets user choose ONE action
 	// Return TRUE if some action is done
 	// Return FALSE if to end session
+	/**
+     * Prints a list of actions and allows the user to choose their actions.
+     * 
+     * @return {@code true} if an action is performed, {@code false} to end the session.
+     */
 	public boolean chooseAction();
 }
