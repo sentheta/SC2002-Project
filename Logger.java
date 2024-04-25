b2373e37f470d051939d5bc5c7ad5456412d0af4
ChuaCleon <164314024+ChuaCleon@users.noreply.github.com>
2024-04-25 21:34:00 +0800

    Update Logger.java

--- Logger.java
+++ Logger.java
@@ -1,12 +1,21 @@
-package FOODIE;
+package scse_FOODIE;
 import java.util.*;
-
-class Logger{
+/**
+ * This class provides functionality for logging messages, verbose mode will be made active once App runs, allowing messages to be printed. 
+ */
+public class Logger{
 	private static boolean active = false;
+	/**
+	 * Activate verbose mode, enabling logging of messages. 
+	 */
 	public static void activate(){
 		System.err.println(" [:] Verbose mode active");
 		active = true;
 	}
+	/**
+	 * Logging a message if verbose mode is active
+	 * @param s String to be logged. 
+	 */
 	public static void log(String s){
 		if(active) System.err.println(" [:] " + s);
 	}
