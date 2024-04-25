07bf01f0354eece9176e87c17bdcc3232cf20e98
ChuaCleon <164314024+ChuaCleon@users.noreply.github.com>
2024-04-25 21:45:37 +0800

    Update PayDebit.java

--- PayDebit.java
+++ PayDebit.java
@@ -1,21 +1,30 @@
-package FOODIE;
+package scse_FOODIE;
 import java.util.*;
 import java.text.*;
 import java.io.Serializable;
-
-class PayDebit extends PayMethod implements Serializable{
+/**
+ * PayDebit class simulates payment by a customer using a debit card.
+ * Class extends PayMethod class. 
+ */
+public class PayDebit extends PayMethod implements Serializable{
 
     //================================================================//
     //================================================================//
-
+    /**
+	 * Default constructor creating a PayDebit object with name debit card.
+	 */
     PayDebit(){
         super("Debit card");
     }
 
-
+    /**
+     * {@inheritDoc}
+     * Allows customers to pay using debit card. 
+     */
     public void pay(double amount){
         Scanner sc = new Scanner(System.in);
         NumberFormat formatter = new DecimalFormat("#0.00");     
+        // Prints payment amount in 2 decimal places.
         System.out.println(formatter.format(amount));
 
         System.out.println("Please tap your card");
