d0f28ba6b046eda1818da701ca812a2183e2b792
ChuaCleon <164314024+ChuaCleon@users.noreply.github.com>
2024-04-25 21:48:44 +0800

    Update PayMobile.java

--- PayMobile.java
+++ PayMobile.java
@@ -1,20 +1,29 @@
-package FOODIE;
+package scse_FOODIE;
 import java.util.*;
 import java.text.*;
 import java.io.Serializable;
-
-class PayMobile extends PayMethod implements Serializable{
+/**
+ * PayMobile class simulates payment by a customer using a mobile method.
+ * Class extends PayMethod class. 
+ */
+public class PayMobile extends PayMethod implements Serializable{
     
     //================================================================//
     //================================================================//
-
+    /**
+	 * Default constructor, creating a PayMobile method with name PayMobile.
+	 */
     PayMobile(){
         super("PayMobile");
     }
-
+    /**
+     * {@inheritDoc}
+     * Method creates a QR code for customers to scan and simulate payment. 
+     */
     public void pay(double amount){
         Scanner sc = new Scanner(System.in);
         NumberFormat formatter = new DecimalFormat("#0.00");     
+        // Display how much to pay.
         System.out.println(formatter.format(amount));
 
         System.out.println("Please scan this QR code on your Mobile");
