3a1c2d1d09ccca69025216368cd896b8b163f49c
ChuaCleon <164314024+ChuaCleon@users.noreply.github.com>
2024-04-25 21:22:37 +0800

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,17 +1,27 @@
-package FOODIE;
+package scse_FOODIE;
 import java.util.*;
 import java.io.Serializable;
-
-class Food implements Serializable{
-    
+/**
+ * This class represents a Food object on the menu
+ */
+public class Food implements Serializable{
+    // Name of food item.
     private String name;
+    // Availability of food item
     private boolean avail; // availability
+    //Price of food item
     private double price;
+    // Category of food item
     private String category;
 
     //================================================================//
     //================================================================//
-
+    /**
+     * Constructs a Food object with the specified attributes
+     * @param name Name of food item
+     * @param price Price of food item
+     * @param category Category of food item
+     */
     public Food(String name,double price,String category){
         Logger.log("Creating new food");
         
@@ -24,13 +34,54 @@ class Food implements Serializable{
     //================================================================//
     //================================================================//
 
- 
+     /**
+     * Gets name of the food item
+     * @return Name of food item
+     */
     public String getName() {return name;}
+    /**
+     * Sets name of food item
+     * @param tmp Name of the food item 
+     */
     public void setName(String tmp) {name = tmp;}
+    /**
+     * Gets the availability of the food item.
+     * @return Availability of food item
+     */
     public boolean getAvail() {return avail;}
+    /**
+     * Sets availability of food item.
+     * @param tmp Availability of food item
+     */
     public void setAvail(boolean tmp) {avail = tmp;}
+    /**
+     * Gets price of food item
+     * @return Price of food item.
+     */
     public double getPrice() {return price;}
+     /**
+     * Sets price of food item. 
+     * @param tmp Price of food item.
+     */
     public void setPrice(double tmp) {price = tmp;}
+    /**
+     * Gets category of food item.
+     * @return Category of food item.
+     */
     public String getCategory() {return category;}
+    /**
+     * Sets category of food item.
+     * @param tmp Category of food item.
+     */
     public void setCategory(String tmp) {category = tmp;}
+    /**
+     * Sets remarks of food item
+     * @param tmp Remarks of food item.
+     */
+    public void setRemarks(String tmp) {remarks = tmp;}
+    /** 
+     * Gets remarks of food item
+     * @return String - remarks of food item.
+     */
+    public String getRemarks() {return remarks;}
 }
