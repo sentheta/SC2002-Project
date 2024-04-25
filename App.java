7bcc3d400dcd81d804004b017ab9a2580522a42a
ChuaCleon <164314024+ChuaCleon@users.noreply.github.com>
2024-04-25 21:01:29 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,25 +1,46 @@
-package FOODIE;
+package scse_FOODIE;
 import java.io.*;
 import java.util.*;
-
-class App implements Serializable{
+/** 
+ * This is the main class of the application. Manages user interaction and control flow of the program. 
+ * Contains methods for handling both user and employee actions, and initializing database by loading and saving data from and to 
+ * a file.
+ */
+public class App implements Serializable{
+	/**
+	 *  ArrayList containing non-admin employees
+	 */
 	public static ArrayList<Employee> employees;	// non-admin employees
+	/**
+	 *  ArrayList containing branches of the company
+	 */
 	public static ArrayList<Branch> branches;
+	/**
+	 *  ArrayList containing different payment methods
+	 */
 	public static ArrayList<PayMethod> payMethods;
-
+	/**
+	 * Constant String filename where we save and load our data from. 
+	 */
 	private static final String filename = "data";
 
 	//================================================================//
     //================================================================//
-
+	/*
+	 * Main method of the application
+	 * Controls the flow of the application by handling user input.
+	 * @param args Command-line arguments. If "verbose" is provided as the first argument,
+         *             verbose mode will be activated.
+	 */
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
-
+		// Activation of Logger
 		if(args.length>=1 && args[0].equals("verbose")){
 			Logger.activate();
 		}
+		// Reading data from file.
 		DataLoader.readFile(filename);
-
+		// Iterates until user exits program
 		while(true){
 			System.out.println();
 			System.out.println("--Choose user--");
@@ -39,11 +60,15 @@ class App implements Serializable{
 			}
 			catch(Exception e){break;}
 		}
-
+		// Writes data to file
 		DataLoader.writeFile(filename);
 		System.out.println("Terminating program...");
 	}
-
+	/**
+	 * Allows user to act as a customer. 
+	 * Prompts user to enter a branch that they are at.
+	 * @see Customer
+	 */
 	public static void beCustomer(){
 		Scanner sc = new Scanner(System.in);
 
@@ -66,10 +91,15 @@ class App implements Serializable{
 		System.out.println("Branch not found");
 
 	}
-
+	/** 
+	 * Allows user to act as an employee
+	 * Prompts user to enter their username and password.
+	 * If employee is verified, they are free to do what they are permitted to do.
+	 * Else, they will not be able to log in to the system.
+	 */
 	public static void beEmployee(){
 		Scanner sc = new Scanner(System.in);
-
+		// Get their username
 		System.out.println("Enter username:");
 		System.out.print(">>> ");
 
@@ -77,8 +107,10 @@ class App implements Serializable{
 			String username = sc.nextLine();
 
 			boolean found = false;
+			// iterates over employees ArrayList to find employee username.
 			for(Employee employee : employees) if(employee.getUsername().equals(username)){
 				found = true;
+				// Verification of password. 
 				if(employee.getAcc().verify(username)){
 					while(employee.chooseAction());
 					break;
