e4361ddd6903474817e4e306131d4df47a72b9e5
80050863+sentheta@users.noreply.github.com <80050863+sentheta@users.noreply.github.com>
2024-04-26 09:09:11 +0800

    a

--- Person.java
+++
@@ -1,7 +0,0 @@
-package FOODIE;
-interface Person{
-	// Print list of action and lets user choose ONE action
-	// Return TRUE if some action is done
-	// Return FALSE if to end session
-	public boolean chooseAction();
-}
